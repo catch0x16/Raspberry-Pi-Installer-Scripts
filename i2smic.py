8560dd63a9c6495da8364ff178720a3d8b77358c
Kevin Dillane <dillane.k@gmail.com>
2024-06-20 16:22:52 -0700

    Second fix

--- i2smic.py
+++ i2smic.py
@@ -36,7 +36,7 @@ Installing...""")
     shell.run_command("apt-get -y install git raspberrypi-kernel-headers")
 
     # Clone the repo
-    shell.run_command("git clone https://github.com/adafruit/Raspberry-Pi-Installer-Scripts.git")
+    shell.run_command("git clone https://github.com/catch0x16/Raspberry-Pi-Installer-Scripts.git")
 
     # Build and install the module
     shell.chdir("Raspberry-Pi-Installer-Scripts/i2s_mic_module")
